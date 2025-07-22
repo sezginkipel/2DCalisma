22672c5b35eb742a282840496d205cc28421094b
Sezgin Kipel <1444sezgin@gmail.com>
2025-07-22 23:13:48 +0300

    Add main menu scene and manager scripts
    Introduces a new MainMenu.unity scene with UI elements, supporting assets, and scripts for menu management. Also adds a visual scripting asset and updates related meta and resource files to enable main menu functionality.

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OyunuBaslat()
+    {
+        Debug.Log("Oyunu Başlat");
+    }
+}
