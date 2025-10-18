eaadc049e25755a1ae9d7ffb97b43b36e4a852d3
Sezgin Kipel <1444sezgin@gmail.com>
2025-10-18 15:24:50 +0300

    Update EnemySpawner.cs

--- Assets/Scripts/Game/EnemySpawner.cs
+++ Assets/Scripts/Game/EnemySpawner.cs
@@ -1,22 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-
-    public GameObject enemyPrefab; // Prefab => 
+    public GameObject enemyPrefab;
     public float spawnInterval = 2f;
+    public float spawnRadius = 5f;
 
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(SpawnEnemies());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnEnemies()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+
+            Vector2 spawnPos = (Vector2)transform.position + Random.insideUnitCircle * spawnRadius;
+            Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius);
     }
 }
