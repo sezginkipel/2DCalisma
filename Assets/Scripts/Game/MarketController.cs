83119d013ece5d37d3ed49de8a3b723fe6bf2a64
Sezgin Kipel <1444sezgin@gmail.com>
2025-10-18 15:25:44 +0300

    Update MarketController.cs

--- Assets/Scripts/Game/MarketController.cs
+++ Assets/Scripts/Game/MarketController.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class MarketController : MonoBehaviour
 {
-    public GameObject marketPanel;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Market Alanına Girdiniz! Alışveriş Yapabilirsiniz.");
-            marketPanel.SetActive(true);
-            Time.timeScale = 0f; // Oyunu duraklat
+            if (GameManager.Instance != null)
+            {
+                Debug.Log("Market Alanına Girdiniz! Alışveriş Yapabilirsiniz.");
+                GameManager.Instance.ChangeState(GameManager.GameState.Paused);
+            }
         }
     }
 }
