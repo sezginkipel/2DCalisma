6e08790a172b87f22d2812ddebc8db5e9ebf7597
Sezgin Kipel <1444sezgin@gmail.com>
2025-10-18 15:24:52 +0300

    Update Enemy.cs

--- Assets/Scripts/Game/Enemy.cs
+++ Assets/Scripts/Game/Enemy.cs
@@ -1,50 +1,90 @@
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+[RequireComponent(typeof(Collider2D))]
+public class Enemy : MonoBehaviour, IDamageable
 {
-    public float health = 100f;
-    public GameObject coinPrefab;
+    public EnemyData enemyData;
+    private float _currentHealth;
+
+    private void Start()
+    {
+        if (enemyData == null)
+        {
+            Debug.LogError("EnemyData is not assigned on " + gameObject.name);
+            enabled = false;
+            return;
+        }
+
+        _currentHealth = enemyData.maxHealth;
+
+        if (EnemyManager.Instance != null)
+        {
+            EnemyManager.Instance.RegisterEnemy(this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (EnemyManager.Instance != null)
+        {
+            EnemyManager.Instance.UnregisterEnemy(this);
+        }
+    }
 
     public void TakeDamage(float damage)
     {
-        health -= damage;
-        // Take damage effect
+        _currentHealth -= damage;
+        
+        // Optional: Add a damage flash effect
+        StartCoroutine(DamageFlash());
+
+        if (_currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private System.Collections.IEnumerator DamageFlash()
+    {
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
         if (sr != null)
         {
             sr.color = Color.red;
-            Invoke("ResetColor", 0.1f);
-        }
-        if (health <= 0f)
-        {
-            DropCoin();
-            Die();
+            yield return new WaitForSeconds(0.1f);
+            sr.color = Color.white;
         }
     }
 
     void Die()
     {
-        // Düşman öldüğünde yapılacak işlemler (örneğin, puan ekleme, efekt oynatma vb.)
+        // TODO: Use enemyData.xpReward
+        DropCoin();
         Destroy(gameObject);
     }
 
-    void ResetColor()
-    {
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        if (sr != null)
-        {
-            sr.color = Color.white;
-        }
-    }
-
-
     void DropCoin()
     {
-        int coinCount = Random.Range(1, 4); // 1 ile 3 arasında rastgele bir sayı
-        for (int i = 0; i < coinCount; i++)
+        // TODO: This should be handled by a LootManager system
+        // For now, we just drop a fixed amount based on data
+        int coinCount = enemyData.goldReward;
+        // This part is problematic as it assumes a coin prefab is known.
+        // This should be moved to a proper loot system later.
+        // if (coinPrefab != null && coinCount > 0) {
+        //     Instantiate(coinPrefab, transform.position, Quaternion.identity);
+        // }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Deal damage to the player on contact
+        if (collision.gameObject.CompareTag("Player"))
         {
-            Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0);
-            Instantiate(coinPrefab, spawnPosition, Quaternion.identity);
+            IDamageable player = collision.gameObject.GetComponent<IDamageable>();
+            if (player != null)
+            {
+                player.TakeDamage(enemyData.damage);
+            }
         }
     }
 }
+
